3a8b339bb12d484be57a9e2e4e58607759ed11cf
Riya130305 <riyakhandelwal4776@gmail.com>
2024-06-01 11:27:15 +0530

    Create Majority Element.java

---
+++ Riya khandelwal/Array/Majority Element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int [] a =new a[]
+        for(int i=0;i<=nums.length;i++)
+            
+{
+            for(int j=0;j<=nums.length;j++)
+
+            {
+               int c=0;
+                if(a[i]==a[j]
+                   {
+                       c=c+1;
+                       
+                   }
+            }
+        }
+            
+    }
+}
+Time complexity O(n2)
+space complexity O(n)
