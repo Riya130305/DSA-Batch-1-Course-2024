4dc2f17db1b8b598ceb81785497e030d24cb9104
Riya130305 <riyakhandelwal4776@gmail.com>
2024-06-05 17:20:41 +0530

    Create Boat_to_save_people.java

---
+++ Riya khandelwal/array/Boat_to_save_people.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        int i=0;
+        int j= people.length-1;
+        int c=0;
+        Arrays.sort(people);
+        while(i<=j)
+        {
+            
+            if(people[i] + people[j] > limit)
+            {
+                j--;
+                c++;
+            }
+            else if(people[i] + people[j]<=limit)
+            {
+                i++;
+                j--;
+                c+=1;
+            }
+           
+        }
+        return c;
+        
+    }
+}
+
+//Time Complexity: O(nlogn)
+
+//Space Complexity: O(logn)
+
